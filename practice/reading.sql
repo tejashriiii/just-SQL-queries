2954ea1198c3b8f23a95fe1380e403944a04b39e
tejashriiii <masketejashri11@gmail.com>
2025-07-28 17:53:32 +0530

    sql queries

---
+++ practice/reading.sql
@@ -0,0 +1,25 @@
+SELECT name
+FROM sqlite_master
+Where type= "table";
+
+SELECT * FROM Album LIMIT 10;
+
+SELECT * FROM Artist LIMIT 10;
+
+SELECT * FROM Customer LIMIT 10;
+-- firstName, LastName, Company, Address, CIty,State,Country,PostalCode, Phone, Fax, Email
+
+SELECT * FROM Employee LIMIT 10;
+--  -"- ,Title, BirthDate, HireDate
+
+SELECT * FROM Genre LIMIT 10;
+
+SELECT * FROM Invoice LIMIT 10;
+-- invoicedate, billingadd and all, total
+
+SELECT * FROM Track LIMIT 10;
+-- composer , milisec, bytesm unitprice
+
+
+
+
