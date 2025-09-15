b362aab8a4256b607ad2b6f16a8e79d93eb2f151
tejashriiii <masketejashri11@gmail.com>
2025-09-15 17:53:32 +0530

    grp ques

---
+++ practice/tut3.sql
@@ -0,0 +1,7 @@
+-- GROUP BY and HAVING
+
+-- 1. Count the number of customers in each country.
+-- 2. Show the total number of invoices for each customer.
+-- 3. Find the total revenue (sum of total) generated from each country.
+-- 4. Find all genres that have more than 100 tracks.
+-- 5. ist employees and how many customers each one supports.
