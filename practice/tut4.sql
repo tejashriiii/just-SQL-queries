8d3fd26c7faa09adf797681996749110be4f779e
tejashriiii <masketejashri11@gmail.com>
2025-09-17 05:02:09 +0530

    nested ques

---
+++ practice/tut4.sql
@@ -0,0 +1,9 @@
+-- 1. Find the names of customers who have made more than 1 invoice.
+
+-- 2. List tracks that are in the playlist named “Grunge”.
+
+-- 3.Find customers who are from the same country as 'Luis Goncalves'.
+
+-- 4. Show all albums that have more than 10 tracks.
+
+-- 5. List all customers who have not made any purchase.
